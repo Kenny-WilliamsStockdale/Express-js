b1a5966427480bf1e41018f89294c7ba3eecb120
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-04 09:43:19 +1200

    added more dependancies

---
+++ src/new-app/public/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+//set static path
+// app.usr(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>'+user+'</h1>');
+})
+
